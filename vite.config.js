368de3c9842332fcc02a743b41f98cef291caebd
Dustin Milam <dustin.milam@cqlcorp.com>
2025-09-11 00:06:26 -0400

    scripts adjustment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/spirit_islant_companion_webapp/", 
+  server: {
+    allowedHosts: [
+      "d6f7e824d660.ngrok-free.app"
+    ]
+  }
 });
